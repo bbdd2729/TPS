cbaa63ded38bd48f7a4b93e626db967de50eb441
bbdd <2729852705@qq.com>
2025-05-17 21:16:48 +0800

    无

--- Assets/Scripts/ScriptableObject/SoundData/SFXData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SFXData", menuName = "Asset/SFX/SFXData")]
-public class SFXData : ScriptableObject
-{
-   [SerializeField] public List<AudioClip> SFXList = new List<AudioClip>();
-}
--- Assets/Scripts/Tool/PoolManager/AudioManager/AudioSO/AudioInventory.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Asset/Audio/AudioInventory")]
-public class AudioInventory : ScriptableObject
-{
-   public List<SFXData> SFXData;
-}
